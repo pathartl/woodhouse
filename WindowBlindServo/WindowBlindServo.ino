c0b7ddd1054e52487672dd24c24b9530974c20c6
Pat Hartl <pat@pathartl.me>
2014-03-10 04:38:12 -0500

    Added absolute rotational movement

--- WindowBlindServo/WindowBlindServo.ino
+++ WindowBlindServo/WindowBlindServo.ino
@@ -6,12 +6,29 @@
 int RECV_PIN = 3;
 int SERVO_PIN = 2;
 int LED_PIN = 13;
+int BLIND_STATUS = 1; // Blinds down
 
 Servo servo;
 
 IRrecv irrecv(RECV_PIN);
 
 decode_results results;
+
+void moveBlinds(int direction) {
+  if (direction == 0) {
+    Serial.println("Blinds up");
+    servo.attach(SERVO_PIN);
+    servo.write(80);
+    delay(1300 * 4);
+    servo.detach();
+  } else if (direction == 1) {
+    Serial.println("Blinds down");
+    servo.attach(SERVO_PIN);
+    servo.write(100);
+    delay(1600 * 4);
+    servo.detach();    
+  }
+}
 
 void setup()
 {
@@ -34,31 +51,45 @@ void loop() {
         break;
       // Blinds up
       case 2:
-        Serial.println("Blinds up");
-        servo.attach(SERVO_PIN);
-        servo.write(80);
-        delay(1300 * 4);
-        servo.detach();
+        // If blinds are open
+        if (BLIND_STATUS == 0) {
+          moveBlinds(0);
+          BLIND_STATUS--;
+        } else if (BLIND_STATUS == 1) {
+        // If blinds are down
+          moveBlinds(0);
+          moveBlinds(0);
+          BLIND_STATUS = -1;
+        }
         break;
       // Blinds open
       case 3:
-        // Stub
+        // If blinds are up
+        if (BLIND_STATUS == -1) {
+          moveBlinds(1);
+          BLIND_STATUS = 0;
+        } else if (BLIND_STATUS == 1) {
+        // Else if blinds are down
+          moveBlinds(0);
+          BLIND_STATUS = 0;
+        }        
         break;
       // Blinds down
       case 4:
-        Serial.println("Blinds down");
-        servo.attach(SERVO_PIN);
-        servo.write(100);
-        delay(1700 * 4);
-        servo.detach();
+        // If blinds are open
+        if (BLIND_STATUS == 0) {
+          moveBlinds(1);
+          BLIND_STATUS++;
+        } else if (BLIND_STATUS == -1) {
+        // Else if blinds are up
+          moveBlinds(1);
+          moveBlinds(1);
+          BLIND_STATUS = 1;
+        }
         break;
-
     }
 
     irrecv.resume(); // Receive the next value
 
   }
-
-
-
 }
