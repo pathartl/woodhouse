b34a217b24da3572eb6553cbb437434c76a90434
Pat Hartl <pat@pathartl.me>
2014-03-11 04:10:34 -0500

    Revamped code for potentiometer
    Added functionality to read a potentiometer for use as an absolute
    positioner

--- WindowBlindServo/WindowBlindServo.ino
+++ WindowBlindServo/WindowBlindServo.ino
@@ -3,10 +3,20 @@
 #include <IRremote.h>
 #include <Servo.h> 
 
+// Pin definitions
 int RECV_PIN = 3;
 int SERVO_PIN = 2;
 int LED_PIN = 13;
-int BLIND_STATUS = 1; // Blinds down
+int POT_PIN = 0;
+
+int BLIND_STATUS = 1; // Deprecated
+
+int numReadings = 10;
+
+// Pot limits
+int POT_MAX = 930;
+int POT_MID = 490;
+int POT_MIN = 20;
 
 Servo servo;
 
@@ -14,10 +24,11 @@ IRrecv irrecv(RECV_PIN);
 
 decode_results results;
 
+// Deprecated
 void moveBlinds(int direction) {
   if (direction == 0) {
     Serial.println("Blinds up");
-    servo.attach(SERVO_PIN);
+    
     servo.write(80);
     delay(1300 * 4);
     servo.detach();
@@ -29,6 +40,62 @@ void moveBlinds(int direction) {
     servo.detach();    
   }
 }
+
+// Reads the potentiometer and smooths it out by doing an average
+int readPot() {
+	int total;
+
+	// Shift array values to left and add to total
+	for (int i = 0; i < numReadings; i++) {
+		total += analogRead(POT_PIN);
+	}
+
+	return total / numReadings;
+
+}
+
+void positionBlinds(int pos) {
+	servo.attach(SERVO_PIN);
+	switch (pos) {
+		case -1:
+			servo.write(80);
+			while (readPot() < POT_MAX) {
+				delay(100);
+			}
+			break;
+		case 0:
+			// A little extra logic for opening because we have to
+			// check what position the blinds are in.
+			// if (readPot() ) {
+			// 	servo.write(80);
+			// } else if (readPot() > )
+
+			// We need to rotate the blinds in the correct direction
+			// Less than the mid should rotate up
+			// More than the mid should rotate down
+			if (readPot() < (POT_MID - 20)) {
+				servo.write(80);
+			}
+
+			if (readPot() > (POT_MID + 20)) {
+				servo.write(100);
+			}
+
+			while ( (readPot() > (POT_MID + 20)) || (readPot() > (POT_MID + 20)) ) {
+				delay(100);
+			}
+			break;
+		case 1:
+			servo.write(100);
+			while (readPot() > POT_MIN) {
+				delay(100);
+			}
+			break;
+	}
+	servo.detach();
+}
+
+
 
 void setup()
 {
@@ -51,41 +118,15 @@ void loop() {
         break;
       // Blinds up
       case 2:
-        // If blinds are open
-        if (BLIND_STATUS == 0) {
-          moveBlinds(0);
-          BLIND_STATUS--;
-        } else if (BLIND_STATUS == 1) {
-        // If blinds are down
-          moveBlinds(0);
-          moveBlinds(0);
-          BLIND_STATUS = -1;
-        }
+        positionBlinds(-1);
         break;
       // Blinds open
       case 3:
-        // If blinds are up
-        if (BLIND_STATUS == -1) {
-          moveBlinds(1);
-          BLIND_STATUS = 0;
-        } else if (BLIND_STATUS == 1) {
-        // Else if blinds are down
-          moveBlinds(0);
-          BLIND_STATUS = 0;
-        }        
+        positionBlinds(0);       
         break;
       // Blinds down
       case 4:
-        // If blinds are open
-        if (BLIND_STATUS == 0) {
-          moveBlinds(1);
-          BLIND_STATUS++;
-        } else if (BLIND_STATUS == -1) {
-        // Else if blinds are up
-          moveBlinds(1);
-          moveBlinds(1);
-          BLIND_STATUS = 1;
-        }
+        positionBlinds(1);
         break;
     }
 
